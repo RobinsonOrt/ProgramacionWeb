540a0725fd737e960505cc31bcd0ae34310961ac
RobinsonOrt <ortix01y@gmail.com>
2020-07-21 14:53:58 -0500

    Nineth Commit

--- Capitulo 4 JS/Trabajo en Clase/JSON AJAX/Ejemplo 1/js/Script_01.js
+++ Capitulo 4 JS/Trabajo en Clase/JSON AJAX/Ejemplo 1/js/Script_01.js
@@ -1,17 +1,33 @@
-function get_productos(){
-	var items = " ";
-	for (var i=1; i<=3; i++){
-		var item =	 '	<div class="media">';
-			item += '  	<img src="Images/Prod_0' + i + '.png" class="align-self-start mr-3" alt="...">';
-			item += '  	<div class="media-body">';
-			item += '	    	<h5 class="mt-0">Producto ' + i + '</h5>';
-			item += '	    	<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>';
-			item += '	    	<p>Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>';
-			item += '  	</div>';
-			item += '	</div>';
+function get_productos_json() {
+	var url = "https://raw.githubusercontent.com/RobinsonOrt/ProgramacionWeb/master/Capitulo%204%20JS/Trabajo%20en%20Clase/Archivos%20Json/Productos.json";
+  	
+  	var xhttp = new XMLHttpRequest();
+  	xhttp.onreadystatechange = function() {
+    	if (this.readyState == 4 && this.status == 200) {
+    	  	console.log(this.responseText);
 
-			items += item;
-	}
-	document.getElementById("Div_Productos").innerHTML = items;
+    	  	var respuesta = this.responseText;
+    	  	var data_json = JSON.parse(respuesta);
+    	  	var productos = data_json.productos;
 
+    	  	console.log(productos);
+
+    	  	var items = "";
+			for (var i=0; i<productos.length; i++){
+			var item =	'<div class="media">';
+				item += '<img src="' + productos[i].foto + '" class="align-self-start mr-3" alt="...">';
+				item += '<div class="media-body">';
+				item += '<h5 class="mt-0">' + productos[i].nombre + '</h5>';
+				item += '<p>' + productos[i].descripcion + '</p>';
+				item += '</div>';
+				item += '</div>';
+
+				items += item;
+			}
+
+    	  	document.getElementById("Div_Productos").innerHTML = items;
+    	}
+  	};
+  	xhttp.open("GET", url, true);
+  	xhttp.send();
 }
---
+++ Capitulo 4 JS/Trabajo en Clase/JSON AJAX/Ejemplo 2/js/Script_01.js
@@ -0,0 +1,34 @@
+function mostrar(respuesta){
+  var data_json = JSON.parse(respuesta);
+  var productos = data_json.productos;
+  var items = "";
+  for (var i=0; i<productos.length; i++){
+  var item =  '<div class="media">';
+    item += '<img src="' + productos[i].foto + '" class="align-self-start mr-3" alt="...">';
+    item += '<div class="media-body">';
+    item += '<h5 class="mt-0">' + productos[i].nombre + '</h5>';
+    item += '<p>' + productos[i].descripcion + '</p>';
+    item += '</div>';
+    item += '</div>';
+
+    items += item;
+  }
+
+  document.getElementById("Div_Productos").innerHTML = items;
+}    
+
+
+function get_productos_json() {
+	var url = "https://raw.githubusercontent.com/RobinsonOrt/ProgramacionWeb/master/Capitulo%204%20JS/Trabajo%20en%20Clase/Archivos%20Json/Productos.json";
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+  	if (this.readyState == 4 && this.status == 200) {
+  	 	console.log(this.responseText);
+
+   		mostrar(this.responseText);
+  	}
+	};
+  	xhttp.open("GET", url, true);
+  	xhttp.send();
+}
